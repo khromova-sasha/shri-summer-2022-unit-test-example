d957f7de7c59df768629534a80dec3c49c221091
sashakhromova <khromova-sasha@inbox.ru>
2022-10-21 15:57:19 +0300

    unit tests

--- src/index.js
+++ src/index.js
@@ -2,26 +2,36 @@ const weights10 = [2, 4, 10, 3, 5, 9, 4, 6, 8, 0];
 const weights12_1 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0];
 const weights12_2 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0];
 
-const calculateControlSum = (weights, inn) => {
-    return weights.reduce((prev, value, i) => prev + value * inn[i]);
-};
+export const weightedSum = (a, w) => {
+    if (w.length !== a.length) {
+        throw new Error("Different array and weights length!");
+    } else {
+        let wSum = 0;
+        for (let i = 0; i < a.length; i++) {
+            if (isNaN(w[i])) {
+                throw new Error("NaN weight!");
+            }
 
-const calculateControlN = (weights, inn) => {
-    return (calculateControlSum(weights, inn) % 11) % 10;
+            if (isNaN(a[i])) {
+                throw new Error("NaN array element!");
+            }
+            wSum += w[i] * a[i];
+        }
+        return wSum;
+    }
 };
 
 export const isInn = (inn) => {
     if (isNaN(Number(inn))) {
         return false;
     } else if (inn.length === 10) {
-        const controlN = calculateControlN(weights10, inn);
+        const controlN = (weightedSum(inn, weights10) % 11) % 10;
         return controlN === Number(inn[9]);
     } else if (inn.length === 12) {
-        const controlN1 = calculateControlN(weights12_1, inn);
-        const controlN2 = calculateControlN(weights12_2, inn);
+        const controlN1 = (weightedSum(inn, weights12_1) % 11) % 10;
+        const controlN2 = (weightedSum(inn, weights12_2) % 11) % 10;
         return controlN1 === Number(inn[10]) && controlN2 === Number(inn[11]);
     } else {
         return false;
     }
-
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,4 +1,61 @@
+import {isInn, weightedSum} from "./index";
 
+describe('weightedSum', () => {
+    it("Throw error if different weights and array length", () => {
+        expect(() => weightedSum([1, 2], [1, 2, 3])).toThrow("Different array and weights length!");
+    });
 
-describe('Тестовая функция, для наглядного примера', () => {
+    it("Throw error if NaN weight", () => {
+        expect(() => weightedSum([1, 2], [3, "NaN"])).toThrow("NaN weight!");
+    });
+
+    it("Throw error if NaN array element", () => {
+        expect(() => weightedSum(["NaN", 4], [3, 1])).toThrow("NaN array element!");
+    });
+
+    it("Correct weighted sum calc", () => {
+        expect(weightedSum([1], [3])).toBe(3);
+        expect(weightedSum([1, 2], [3, 4])).toBe(11);
+        expect(weightedSum([1, -2], [3, 4])).toBe(-5);
+    });
+
+    it("String instead of array", () => {
+        expect(weightedSum("1", [3])).toBe(3);
+        expect(weightedSum("12", [3, 4])).toBe(11);
+        expect(weightedSum("123", [3, 4, 5])).toBe(26);
+
+        expect(weightedSum([1], "3")).toBe(3);
+        expect(weightedSum([1, 2], "34")).toBe(11);
+        expect(weightedSum([1, 2, 3], "345")).toBe(26);
+
+        expect(weightedSum("1", "3")).toBe(3);
+        expect(weightedSum("12", "34")).toBe(11);
+        expect(weightedSum("123", "345")).toBe(26);
+    });
+});
+
+describe("isInn", () => {
+    it ("Correct inn", () => {
+        expect(isInn("1111111117")).toBeTruthy();
+        expect(isInn("7743013901")).toBeTruthy();
+    });
+
+    it("Wrong characters in inn", () => {
+        expect(isInn("aa3343434")).toBeFalsy();
+        expect(isInn("12k2333")).toBeFalsy();
+        expect(isInn("123-=dd")).toBeFalsy();
+        expect(isInn(" 7743013901")).toBeFalsy();
+    });
+
+    it("Wrong inn length", () => {
+        expect(isInn("1")).toBeFalsy();
+        expect(isInn("11111")).toBeFalsy();
+        expect(isInn("11111111111")).toBeFalsy();
+        expect(isInn("11111111111111")).toBeFalsy();
+    });
+
+    it ("Incorrect inn", () => {
+        expect(isInn("1111211117")).toBeFalsy();
+        expect(isInn("7743013902")).toBeFalsy();
+    });
 });
